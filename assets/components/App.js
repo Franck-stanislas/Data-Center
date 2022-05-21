1059c9d4c64afa38e968132705435fca524cf471
AIR ALPHA <allphaair@gmail.com>
2022-05-22 01:41:45 +0200

    project step 2

--- assets/components/App.js
+++ assets/components/App.js
@@ -25,39 +25,160 @@ const App = () => {
                     <h2 className="h4">Nouveau projet</h2>
                     <p className="mb-0">Suivez ces etapes.</p>
                 </div>
-                <div className="btn-toolbar mb-2 mb-md-0">
-                    <a href="#" className="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
-                        <svg className="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                        </svg>
-                        New Plan
-                    </a>
-                    <div className="btn-group ms-2 ms-lg-3">
-                        <button type="button" className="btn btn-sm btn-outline-gray-600">Share</button>
-                        <button type="button" className="btn btn-sm btn-outline-gray-600">Export</button>
-                    </div>
-                </div>
             </div>
             <Stepper active={step} breakpoint="lg" onStepClick={setStep}>
                 <Stepper.Step
                     label=""
                     completedIcon={<i className='bx bx-map-pin bx-tada-hover bx-md'/>}
+                    icon={<i className='bx bx-map-pin bx-tada-hover bx-md'/>}
                     loading={step === 0}
                 />
                 <Stepper.Step
                     label=""
+                    allowStepClick={false}
                     completedIcon={<i className='bx bx-info-circle bx-tada-hover bx-md'/>}
+                    icon={<i className='bx bx-info-circle bx-tada-hover bx-md'/>}
                     loading={step === 1}
                 />
                 <Stepper.Step
                     label=""
                     completedIcon={<i className='bx bx-dots-horizontal-rounded bx-tada-hover bx-md'/>}
+                    icon={<i className='bx bx-dots-horizontal-rounded bx-tada-hover bx-md'/>}
                     loading={step === 2}
                 />
             </Stepper>
 
+            {step === 0 && <form className="container">
+                <div className="d-block mt-4 mb-md-0">
+                    <p className="mb-0">Origine du projet.</p>
+
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Region</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Département</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Arrondissement</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Commune</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+            </form>}
+
+            {step === 1 && <form>
+                <div className="d-block mt-4 mb-md-0">
+                    <p className="mb-0">Présentation du projet.</p>
+
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Intitulé</label>
+                        <input type="text" className="form-control" id="email"/>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Secteur</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Objectis</label>
+                        <textarea className="form-control" placeholder="Enter your message..." id="textarea"
+                                  rows="4"></textarea>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Resultats attendus</label>
+                        <textarea className="form-control" placeholder="Enter your message..." id="textarea"
+                                  rows="4"></textarea>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Coût estimatif</label>
+                        <input type="number" className="form-control" id="email"/>
+                    </div>
+                </div>
+            </form>}
+
+            {step === 2 && <form className="container">
+                <div className="d-block mt-4 mb-md-0">
+                    <p className="mb-4">Autres informations.</p>
+
+                    <div className="row container">
+                        <div className="form-check col-lg-4 col-sm-6">
+                            <input className="form-check-input" type="checkbox" value="" id="defaultCheck10" />
+                            <label className="form-check-label" htmlFor="defaultCheck10">
+                                Idée de projet
+                            </label>
+                        </div>
+                        <div className="form-check col-lg-4 col-sm-6">
+                            <input className="form-check-input" type="checkbox" value="" id="defaultCheck10" />
+                            <label className="form-check-label" htmlFor="defaultCheck10">
+                                En cours de maturité
+                            </label>
+                        </div>
+                        <div className="form-check col-lg-4 col-sm-6">
+                            <input className="form-check-input" type="checkbox" value="" id="defaultCheck10" />
+                            <label className="form-check-label" htmlFor="defaultCheck10">
+                                Mature
+                            </label>
+                        </div>
+                    </div>
+
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Intitulé</label>
+                        <input type="text" className="form-control" id="email"/>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Secteur</label>
+                        <select className="form-select" id="country" aria-label="Default select example">
+                            <option selected="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Objectis</label>
+                        <textarea className="form-control" placeholder="Enter your message..." id="textarea"
+                                  rows="4"></textarea>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Resultats attendus</label>
+                        <textarea className="form-control" placeholder="Enter your message..." id="textarea"
+                                  rows="4"></textarea>
+                    </div>
+                    <div className="mb-4 mt-2">
+                        <label className="my-1 me-2" htmlFor="country">Coût estimatif</label>
+                        <input type="number" className="form-control" id="email"/>
+                    </div>
+                </div>
+            </form>}
         </>
     );
 };
